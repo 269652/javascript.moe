08f4ddd7b71cbbf92d9b244ddc2689ffe9d148c8
Moritz Roessler <morrossl@publicisgroupe.net>
2025-05-15 19:29:37 +0200

    feat: improve language switcher desing

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -4,15 +4,20 @@ import { useParams, usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import supportedLocales from "@/lib/locales"; // Adjust path as needed
+import clsx from "clsx";
 
-const LanguageSwitcher = ({ availableLocales, href, showCurrent }: any) => {
+const LanguageSwitcher = ({
+  availableLocales,
+  href,
+  showCurrent = true,
+}: any) => {
   const { locale } = useParams<{ locale: string }>();
   const pathname = usePathname();
 
   const currentLocale = supportedLocales.includes(locale) ? locale : "en";
 
   return (
-    <div className="top-4 right-8 flex gap-2 z-10 h-fit">
+    <div className="top-4 right-8 flex z-10 h-fit rounded-md bg-black/30 overflow-hidden ">
       {supportedLocales
         .filter(
           (loc) =>
@@ -24,13 +29,15 @@ const LanguageSwitcher = ({ availableLocales, href, showCurrent }: any) => {
           const newUrl = path.replace(`/${currentLocale}`, `/${loc}`);
 
           return (
-            <Link key={loc} href={newUrl} locale={loc}>
+            <Link key={loc} href={newUrl}>
               <Image
                 src={`/images/flags/${loc}.png`}
                 alt={`Switch to ${loc}`}
                 width={30}
                 height={20}
-                className="cursor-pointer min-w-[32px]"
+                className={clsx("cursor-pointer min-w-[48px] p-2 hover:bg-white/15", {
+                  "cursor-default  bg-white/20 shadow-[0px_0px_3px_1px_black_inset]": currentLocale === loc,
+                })}
               />
             </Link>
           );
--- src/components/pages/BlogPage.tsx
+++ src/components/pages/BlogPage.tsx
@@ -56,24 +56,26 @@ export default async function BlogPage({ params, searchParams }: any) {
         />
         <div className="block w-full justify-center h-screen overflow-y-auto p-1 md:p-4 mx-auto">
           <main className="bg-black/40 w-full mx-auto p-2 md:p-4 drop-shadow-2xl flex flex-col max-w-7xl">
-            <div className="flex justify-between">
-              <h1 className="mb-4 text-3xl font-bold">Mo's Blog</h1>
-              <LanguageSwitcher availableLocales={supportedLocales} />
-            </div>
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between">
+                <h1 className="mb-4 text-3xl font-bold">Mo's Blog</h1>
+                <LanguageSwitcher availableLocales={supportedLocales} />
+              </div>
 
-            {/* Labels */}
-            <Labels
-              labels={labels}
-              labelNames={labelNames}
-              className="flex md:hidden"
-            />
-            <div className="flex justify-between h-fit items-end">
-              <Categories categories={categories} activeSlug={categorySlug} />
+              {/* Labels */}
               <Labels
                 labels={labels}
                 labelNames={labelNames}
-                className="hidden md:flex"
+                className="flex md:hidden"
               />
+              <div className="flex justify-between h-fit items-end">
+                <Categories categories={categories} activeSlug={categorySlug} />
+                <Labels
+                  labels={labels}
+                  labelNames={labelNames}
+                  className="hidden md:flex"
+                />
+              </div>
             </div>
 
             {posts.length === 0 ? (
